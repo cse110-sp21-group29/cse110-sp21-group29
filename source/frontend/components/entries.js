5a7759475c00b521729709fceb7b09d4b8533a1a
jtran <jtran2000@outlook.com>
2021-05-13 12:19:04 -0700

    Continuing on Daily Log

---
+++ source/frontend/components/entries.js
@@ -0,0 +1,26 @@
+class Entries extends HTMLElement {
+  constructor () {
+    super();
+    this.attachShadow({ mode: 'open' });
+    const template = document.createElement('template');
+    template.innerHTML = `
+          <link rel="stylesheet" href="../styles/bootstrap.css">
+          <ul class=""list-group"></ul>
+      `;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    const list = this.shadowRoot.querySelector('ul');
+    // this.classList.add('card');
+  }
+
+  get content () {
+    return this.getAttribute('content');
+  }
+
+  set content (content) {
+    content.forEach((entry) => {
+
+    });
+  }
+}
+
+customElements.define('entries', Entries);
--- source/frontend/dailyLog/scripts/script.js
+++ source/frontend/dailyLog/scripts/script.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
   const url = './test.json'; // SET URL
+  const main = document.querySelector('main');
   fetch(url)
     .then(response => response.json()) /* FILL IN RESPONSE HANDLING HERE */
     .then(days => {
-      days.forEach((entry) => {
-        const newDay = document.createElement('journal-entry');
-
-        newDay.entry = entry;
-        document.body.appendChild(newDay);
+      days.forEach((day) => {
+        /*  const newDay = document.createElement('day-log');
+        newDay.content = content;
+        newDay.classList.add('card');
+        document.body.appendChild(newDay); */
+        const newDay = document.createElement('section');
+        newDay.classList.add('card');
+        const date = new Date(day.date);
+        const dateTitle = date.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' });
+        newDay.innerHTML = '<div class="card-body"><h2 class="card-title text-center"><time datetime="' + day.date + '">' + dateTitle + '</time></h2></div>';
+        main.appendChild(newDay);
       });
     })
     .catch(error => {

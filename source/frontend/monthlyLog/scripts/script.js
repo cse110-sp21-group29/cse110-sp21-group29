b177a78506fd4e4210ee847648d14509e222b2b1
anahitaafsh <47436110+anahitaafsh@users.noreply.github.com>
2021-05-13 23:11:51 -0700

    more lint fixes

--- source/frontend/monthlyLog/scripts/script.js
+++ source/frontend/monthlyLog/scripts/script.js
@@ -1,4 +1,3 @@
-// Get Month 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const d = new Date();
 const n = months[d.getMonth()];
@@ -7,15 +6,15 @@ document.getElementById('monthName').innerHTML = n;
 document.getElementById('yearName').innerHTML = y;
 
 // Get Days
-function getDaysInMonth(month, year) {
-  let date = new Date(year, month, 1);
-  let days = [];
+function getDaysInMonth (month, year) {
+  const date = new Date(year, month, 1);
+  const days = [];
   const dNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   while (date.getMonth() === month) {
-    let temp = new Date(date);
-    let tempDayName = dNames[temp.getDay()];
-    let tempDayNum = temp.getDate();
-    let tempStr = String(tempDayNum) + '         ' + String(tempDayName);
+    const temp = new Date(date);
+    const tempDayName = dNames[temp.getDay()];
+    const tempDayNum = temp.getDate();
+    const tempStr = String(tempDayNum) + '         ' + String(tempDayName);
     days.push(tempStr);
     date.setDate(date.getDate() + 1);
   }
@@ -24,8 +23,8 @@ function getDaysInMonth(month, year) {
 
 const allDays = getDaysInMonth(d.getMonth(), y);
 
-function makeList() {
-  let listData = allDays;
+function makeList () {
+  const listData = allDays;
   const listContainer = document.createElement('div');
   const listElement = document.createElement('ul');
   const numberOfListItems = listData.length;
@@ -36,12 +35,12 @@ function makeList() {
   listContainer.appendChild(listElement);
 
   for (i = 0; i < numberOfListItems; i++) {
-    let count = listData[i];
-    let weekCount = count.slice(count.length - 3);
+    const count = listData[i];
+    const weekCount = count.slice(count.length - 3);
     console.log(weekCount);
 
     if (weekCount === 'Sun') {
-      let elem = document.createElement('hr');
+      const elem = document.createElement('hr');
       listElement.appendChild(elem);
       console.log(weekCount);
     }

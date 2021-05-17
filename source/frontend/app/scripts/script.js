ca85b9a7759f6b90c1a8c412425427c6c34495bb
jtran <jtran2000@outlook.com>
2021-05-16 17:44:20 -0700

    Continuing to turn Daily Log into web comp

--- source/frontend/app/scripts/script.js
+++ source/frontend/app/scripts/script.js
@@ -1,10 +1,12 @@
+
 document.addEventListener('DOMContentLoaded', () => {
   const url = './test.json'; // SET URL
   const dailyLog = document.querySelector('daily-log');
   fetch(url)
-    .then(response => response.json()) /* FILL IN RESPONSE HANDLING HERE */
+    .then(response => response.json())
     .then(days => {
       dailyLog.days = days;
+      console.log(days);
     })
     .catch(error => {
       console.log(`%cresult of fetch is an error: \n"${error}"`, 'color: red');

dee4c6bf70de7649d7d848fe79d0a1784d74c74b
elew27 <elew@ucsd.edu>
2021-05-19 17:35:10 -0700

    fixed some linting

--- source/backend/website/bulletjournal/views.py
+++ source/backend/website/bulletjournal/views.py
@@ -1,24 +1,25 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+#from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
-from .forms import LoginForm, SignUpForm
+#from django.contrib.auth.models import User
+from .forms import LoginForm
 from django.contrib.auth.forms import UserCreationForm
+
+
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            #user = User.objects.create_user(username = form.cleaned_data.get('user_name'), password = form.cleaned_data.get('pword'))
-            #user.save()
             user = authenticate(username=form.cleaned_data.get('username'), password=form.cleaned_data.get('password1'))
             login(request, user)
             return HttpResponseRedirect('front_page')
     else:
         form = UserCreationForm()
     return render(request, "SignUpPage.html", {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -31,6 +32,7 @@ def login_view(request):
     else:
         form = LoginForm()
     return render(request, "LoginPage.html", {'form': form})
+
 
 def front_page(request):
     return render(request, "FrontPage.html")

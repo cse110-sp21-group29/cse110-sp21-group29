3476fcd832d90f0f6d4dc4cd72ab6cbeb2b2d7dd
Dadian Zhu <zhu1258796894@hotmail.com>
2021-05-23 15:10:11 -0700

    linting

--- source/frontend/components/sidebar.js
+++ source/frontend/components/sidebar.js
@@ -1,8 +1,8 @@
-class SideBar extends HTMLElement{
-    constructor(){
-        super();
-        const template=document.createElement('template');
-        template.innerHTML=`
+class SideBar extends HTMLElement {
+  constructor () {
+    super();
+    const template = document.createElement('template');
+    template.innerHTML = `
             <style>
             
             .sidebar {
@@ -27,37 +27,36 @@ class SideBar extends HTMLElement{
              </section>
             
             `;
-        this.attachShadow({mode : 'open'});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-            
-    }
-    get content(){
-        return this.shadowRoot.querySelector("[class='sidebar']");
-    }
-    set content(entry){
-        const futureList=document.createElement('ul');
-        const monthlyList=document.createElement('ul');
-        const dailyList=document.createElement('ul');
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
 
-        // entries.forEach(entry=>{
-            const length = document.querySelectorAll('time').length;
-            const li=document.createElement('li');
-            li.innerHTML=document.querySelectorAll('time')[length-1].innerHTML;
-            // if(entry.category=='Future Log'){
-            //     futureList.appendChild(li);
-            // }
-            // if(entry.category=='Monthly Log'){
-            //     monthlyList.appendChild(li);
-            // }
-            // if(entry.category=='Daily Log'){
-                dailyList.appendChild(li);
-            // }
-        // });  
-        this.shadowRoot.querySelector("[class='futureLog']").appendChild(futureList);
-        this.shadowRoot.querySelector("[class='monthlyLog']").appendChild(monthlyList);
-        this.shadowRoot.querySelector("[class='dailyLog']").appendChild(dailyList);
+  get content () {
+    return this.shadowRoot.querySelector("[class='sidebar']");
+  }
 
-    }
-    
+  set content (entry) {
+    const futureList = document.createElement('ul');
+    const monthlyList = document.createElement('ul');
+    const dailyList = document.createElement('ul');
+
+    // entries.forEach(entry=>{
+    const length = document.querySelectorAll('time').length;
+    const li = document.createElement('li');
+    li.innerHTML = document.querySelectorAll('time')[length - 1].innerHTML;
+    // if(entry.category=='Future Log'){
+    //     futureList.appendChild(li);
+    // }
+    // if(entry.category=='Monthly Log'){
+    //     monthlyList.appendChild(li);
+    // }
+    // if(entry.category=='Daily Log'){
+    dailyList.appendChild(li);
+    // }
+    // });
+    this.shadowRoot.querySelector("[class='futureLog']").appendChild(futureList);
+    this.shadowRoot.querySelector("[class='monthlyLog']").appendChild(monthlyList);
+    this.shadowRoot.querySelector("[class='dailyLog']").appendChild(dailyList);
+  }
 }
-customElements.define('side-bar',SideBar);
+customElements.define('side-bar', SideBar);

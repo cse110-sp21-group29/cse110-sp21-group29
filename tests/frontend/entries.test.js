1a5f8ec64c2ed46459858ed7c0c2afadcd19596a
jtran <jtran2000@outlook.com>
2021-06-04 16:57:03 -0700

    Fixed syntax error

--- tests/frontend/entries.test.js
+++ tests/frontend/entries.test.js
@@ -1,4 +1,4 @@
 import {Entries} from '../../source/frontend/components/entries'
-test('Will finish this test later', () =< {
+test('Will finish this test later', () => {
     expect(1+1).toBe(2);
 });

a7410321e8d5d4c03f7f6cc28257d91210803aa7
jtran <jtran2000@outlook.com>
2021-06-10 17:59:29 -0700

    Deleting test

--- tests/frontend/router.test.js
+++
@@ -1,5 +0,0 @@
-import { router } from '../../source/frontend/app/scripts/router';
-test('setState works and defaults to Daily Log', () => {
-    router.setState();
-    expect(location.hash).toBe("#/dailyLog");
-  });
